2439a1bae8c8047723a9c2a854f7faad6f96c055
Colorado Montolio <jorge.colorado-montolio@capgemini.com>
2022-08-05 08:39:26 +0200

    Changed domain module to android module

---
+++ domain/src/test/java/com/marvel/domain/GetCharacterByIdUseCaseTest.kt
@@ -0,0 +1,25 @@
+package com.marvel.domain
+
+import com.marvel.model.Character
+import com.marvel.model.Result
+import com.marvel.repositories.CharactersRepository
+import com.marvel.usecases.GetCharacterByIdUseCaseImpl
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.doReturn
+
+class GetCharacterByIdUseCaseTest {
+    private val mockedCharactersRepo = Mockito.mock(CharactersRepository::class.java)
+    private val fakeCharacter = Character(0, "", "", "")
+    private val fakeResult = Result.Success(fakeCharacter)
+
+    @Test
+    fun `should return single Character by id`() = runTest {
+        doReturn(fakeResult).`when`(mockedCharactersRepo).getCharacterById(0)
+
+        val character = GetCharacterByIdUseCaseImpl(mockedCharactersRepo)(0)
+        Assert.assertEquals(character, fakeResult)
+    }
+}
---
+++ domain/src/test/java/com/marvel/domain/GetCharactersTest.kt
@@ -0,0 +1,33 @@
+package com.marvel.domain
+
+import com.marvel.model.Character
+import com.marvel.repositories.CharactersRepository
+import com.marvel.usecases.GetCharactersUseCase
+import com.marvel.model.Result
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.doReturn
+
+
+class GetCharactersTest {
+
+    private val mockedCharactersRepo = Mockito.mock(CharactersRepository::class.java)
+    private val fakeCharacter = Character(0, "", "", "")
+    private val fakeCharacters: List<Character> = listOf(fakeCharacter)
+
+    private val initialSize = 50
+    private val initialSkip = 0
+    private val getCharacters = GetCharactersUseCase(mockedCharactersRepo)
+
+    @Test
+    fun `should return Result class with list of Characters`() = runTest {
+        doReturn(Result.Success(fakeCharacters)).`when`(mockedCharactersRepo)
+            .getCharacters(initialSize, initialSkip)
+        val expectedResult = Result.Success(fakeCharacters)
+
+        val charactersResult = getCharacters(initialSize, initialSkip)
+        Assert.assertEquals(expectedResult, charactersResult)
+    }
+}
